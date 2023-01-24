fc164e09e8ffc2bef6e213aa2bc97f12d0666c83
tutorial-san <tutorialsan@proton.me>
2023-01-24 16:42:42 -0500

    the change is one line on 27 that victory on jose  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn about Victory Temple here
+          Learn about Jose Temple here
         </a>
       </header>
     </div>
