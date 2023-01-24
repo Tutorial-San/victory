0bd9e69844db761047fee26956390a7dedf2b76d
tutorial-san <tutorialsan@proton.me>
2023-01-24 15:45:27 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://victorytemple.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about Victory Temple here
         </a>
       </header>
     </div>
